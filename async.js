1483cf63c34712d6c418e063b9dda1153e88f235
RafalyAhmad <136140993+RafalyAhmad@users.noreply.github.com>
2025-12-31 14:39:03 +0700

    Update async.js

--- async.js
+++ async.js
@@ -6,3 +6,13 @@ const tambah = (a,b) => a+b;
 const kali = (a,b)=> a*b;
 
 console.log(hitung(10,5,tambah))
+
+
+//studi kasus 2
+function prosesPesanan(namaMakanan,callback){
+    console.log(`Memproses pesanan: ${namaMakanan}`)
+    callback()
+}
+function notifikasiSelesai(){console.log("Pesanan sudah siap! Silakan diambil")}
+
+prosesPesanan("ayam geprek",notifikasiSelesai)
